87a7a80c7ffcd06a12c25443a2529cd9d6787a73
Aditya Mathur <adityamathur2018@gmail.com>
2021-04-20 12:28:56 +0530

    Adding introduction resampling and parsnip

---
+++ Machine Learning With Tidy Models.R
@@ -0,0 +1,129 @@
+library(tidymodels)
+
+#TidyModels is a collection of Libraries/pachages for streamlined Machine Learning Workflow in R
+
+
+home_sales<- read.csv("Data/train.csv")
+
+
+
+
+
+
+
+# Step 1:Data Resampling -- rsample---- 
+
+##  The rsample package is designed to create training and test datasets.
+## Creating a test dataset is important for estimating how a trained model will likely perform on new data.
+## It also guards against overfitting, where a model memorizes patterns that exist only in the training data 
+## and performs poorly on new data.  
+
+home_split <- initial_split(home_sales,
+                      prop =0.70 ,
+                      strata = SalePrice)
+# here we are creating an rsample object, home_split usinf initial_split() 
+#function that contains instructions for randomly splitting home_sales data into training and test data
+
+# Create the training data
+home_training <- home_split %>%
+  training()
+
+# Create the test data
+home_test <- home_split %>% 
+  testing()
+
+# Check number of rows in each dataset
+nrow(home_training)
+nrow(home_test)
+
+
+
+# 1.1 Distribution of outcome variable values----
+#Stratifying by the outcome variable when generating training and test datasets
+#ensures that the outcome variable values have a similar range in both datasets.
+
+
+#Since the original data is split at random, 
+#stratification avoids placing all the expensive homes in home_sales into the test dataset
+
+#Let's Calculate summary statistics for the selling_price variable in the training
+# and test datasets. for the home_training and home_test
+
+# Distribution of selling_price in training data
+home_training %>% 
+  summarize(min_sell_price = min(SalePrice),
+            max_sell_price = max(SalePrice),
+            mean_sell_price = mean(SalePrice),
+            sd_sell_price = sd(SalePrice))
+
+# Distribution of selling_price in test data
+home_test %>% 
+  summarize(min_sell_price = min(SalePrice),
+            max_sell_price = max(SalePrice),
+            mean_sell_price = mean(SalePrice),
+            sd_sell_price = sd(SalePrice))
+
+
+# The minimum and maximum selling prices in both datasets are the same.
+# The mean and standard deviation are also similar.
+# Stratifying by the outcome variable ensures the model fitting process is performed on
+# a representative sample of the original data.
+
+
+
+# Prediction of Sales Price of Houses is a Regression Problem.
+
+# Step 2: Model Formulas -- Parsnip----
+
+
+# 2.1 Specify a linear regression model, linear_model----
+linear_model <- linear_reg() %>% 
+  # Set the model engine
+  set_engine('lm') %>% 
+  # Set the model mode
+  set_mode('regression')
+
+# 2.2 Train the model with the training data----
+lm_fit <- linear_model %>% 
+  fit(SalePrice ~LotArea+YrSold,
+      data = home_training)
+
+# Print lm_fit to view model information
+lm_fit
+
+# have defined your model with linear_reg() and 
+#trained it to predict selling_price using Yrsold and LotArea. 
+#Printing a parsnip model fit object displays useful model information,
+#such as the training time, model formula used during training, and
+#the estimated model parameters.
+
+
+#The tidy() function automatically creates a tibble of estimated model parameters.
+#Since LotArea has a positive estimated parameter,
+#the selling price of homes increases with the square footage.
+#Conversely, since YrSold has a negative estimated parameter,
+#older homes tend to have lower selling prices.
+
+# 2.3  Predict selling_price----
+home_predictions <- predict(lm_fit,
+                            new_data = home_test)
+
+# View predicted selling prices
+home_predictions
+
+#2.4  Combine test data with predictions----
+home_test_results <- home_test %>% 
+  select(SalePrice,YrSold,LotArea) %>% 
+  cbind(home_predictions)
+
+# View results
+home_test_results
+
+
+#have trained a linear regression model and used it to predict the selling prices of homes
+#in the test dataset! 
+#The model only used two predictor variables, 
+#but the predicted values in the .pred column seem reasonable!
+
+
+# Step 3: Evaluating Model Performance ----
