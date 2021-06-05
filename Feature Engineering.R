54899f3a18c8f0b6d7c7adb0c0aedad81834a617
Aditya Mathur <adityamathur2018@gmail.com>
2021-06-05 08:25:03 +0530

    Adding Feature Engineering: *Multicolinearity *Normalization to Numerical Predictors

--- Feature Engineering.R
+++ Feature Engineering.R
@@ -60,3 +60,78 @@ telecom_log_rec_prep %>%
 # Apply to test data
 telecom_log_rec_prep %>% 
   bake(new_data = telecom_test)
+
+
+# Numeric Predictors ----
+
+# Correlation Matrix:
+telecom_train %>% select_if(is.numeric) %>% 
+  cor() %>% View()
+
+# there is  correlation between Monthly Charge and data usage
+##########################################################################################################################################################################################################################################################################################################################################
+##  Correlated predictor variables provide redundant information and can negatively
+## impact the model fitting process. When two variables are highly correlated, their 
+## values change linearly with each other and hence provide the same information to 
+## your machine learning algorithms. This phenomenon is know as multicollinearity.  ##
+##########################################################################################################################################################################################################################################################################################################################################
+
+
+
+# Plot correlated predictors
+ggplot(telecom_train, aes(x = DataUsage, y = MonthlyCharge)) + 
+  # Add points
+  geom_point() +
+# Add title
+labs(title ="Monthly Charges vs. Average Data Usage",
+     y = 'Monthly Charges ($)', x = 'Average Data Usage (GB)')
+
+# Its good to remove the multicollinearity 
+
+# Specify a recipe object
+telecom_cor_rec <- recipe(Churn ~ .,
+                          data = telecom_train) %>% 
+  # Remove correlated variables
+  step_corr(all_numeric(), threshold = 0.8)
+
+
+# Train the recipe
+telecom_cor_rec_prep <- telecom_cor_rec %>% 
+  prep(training = telecom_train
+       
+       )
+
+
+# Apply to training data
+telecom_cor_rec_prep %>% 
+  bake(new_data = NULL)
+
+# Apply to test data
+telecom_cor_rec_prep %>% 
+  bake(new_data = telecom_test)
+
+
+#################################################################################################################################################################################################################
+##  The power of the recipes package is that you can include multiple preprocessing
+## steps in a single recipe object. These steps will be carried out in the order
+## they are entered with the step_*() functions  ##
+##################################################################################################################################################################################################################
+
+# Specify a recipe object
+telecom_norm_rec <- recipe(Churn ~ .,
+                           data = telecom_train) %>% 
+  # Remove correlated variables
+  step_corr(all_numeric(), threshold = 0.8) %>% 
+  # Normalize numeric predictors
+  step_normalize(all_numeric())
+
+# Train the recipe
+telecom_norm_rec_prep <- telecom_norm_rec %>% 
+  prep(training = telecom_train)
+
+# Apply to test data
+telecom_norm_rec_prep %>% 
+  bake(new_data = telecom_test)
+
+
+# Nominal Predictors----
